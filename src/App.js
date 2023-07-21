d3b4122b9c4666cfb0a92f80658b8fd370540177
Rifat <rifatalom97@gmail.com>
2023-07-21 22:20:47 +0600

    ostad react todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const removeHandler = (selectedTask) => {
+     const updatedTasks = tasks.filter((item)=>item!=selectedTask);
+
+     setTasks(updatedTasks);
+  }
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+
+    if(task.length){
+      setTasks([
+        ...tasks,
+        task
+      ]);
+      setTask('');
+    }
+  }
+
+  const handleTaskChange = (e) => {
+    let val = e.target.value;
+    if(val.length){
+      
+      setTask(val);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="list_item_adder">
+          <form onSubmit={handleAddTask}>
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              value={task}
+              onChange={handleTaskChange}
+              placeholder="Enter a new task..."
+            />
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+
+        <div className="list_items">
+          <h3>All Tasks</h3>
+
+          <ul>
+            {tasks.length?
+              tasks.map((item,i)=>{
+                return <li key={i}>{item} <button type="button" onClick={()=>removeHandler(item)}>Remove</button></li>
+              })
+            :
+              <li>No tasks found</li>
+            }
+          </ul>
+        </div>
     </div>
   );
 }
